64be0506f0eeb3d80123ca2e3c5dddd02c3545f5
Ben Dwyer <17102207+bendwyer@users.noreply.github.com>
2024-10-20 16:49:19 +0200

    Add terraform shell tab-completion (#23)
    * Add terraform shell tab-completion
    
    * Update short version scenario and tests

--- test/terraform/terraform_version_short_ubuntu.sh
+++ test/terraform/terraform_version_short_ubuntu.sh
@@ -10,9 +10,8 @@ source dev-container-features-test-lib
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
 echo ""
-echo "Running as $(whoami)"
-check "version" terraform --version
-check "terraform-bash-completion-contains-version-option" ./check_bash_completion.sh "terraform " "version"
+echo "Current user is $(whoami)"
+check "terraform version" terraform --version
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
