c13ee07fb1e8a933b06df7fb0a493b9c97af1629
bendwyer <17102207+bendwyer@users.noreply.github.com>
2025-08-23 20:35:25 +0000

    Remove single quotes

--- src/flux/install.sh
+++ src/flux/install.sh
@@ -12,7 +12,7 @@ echo "Checking if curl is installed..."
 if type curl > /dev/null 2>&1; then
     echo "curl already installed. Skipping..."
 else
-  echo "Installing curl...'"
+  echo "Installing curl..."
   apt-get -yq update
   apt-get -yq install curl
   echo "curl installation complete!"
@@ -22,7 +22,7 @@ echo "Checking if flux is installed..."
 if [ "${FLUX_VERSION}" = "none" ] || type flux > /dev/null 2>&1; then
     echo "flux already installed. Skipping..."
 else
-  echo "Installing flux...'"
+  echo "Installing flux..."
   if [ "${FLUX_VERSION}" = "latest" ] ; then
     curl -s https://fluxcd.io/install.sh | bash
   else
