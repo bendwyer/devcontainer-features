ae78951fee6431b4e12a7c11c855adb86ecae4fc
Ben Dwyer <17102207+bendwyer@users.noreply.github.com>
2025-12-22 01:07:26 +0100

    Update install script internal vars (#70)

--- src/terraform-mcp-server/install.sh
+++ src/terraform-mcp-server/install.sh
@@ -3,31 +3,24 @@
 set -e
 
 VERSION="${VERSION:-latest}"
-feature_name="terraform-mcp-server"
+binary_name="terraform-mcp-server"
 
-if [ $(uname -m) = "aarch64" ] || [ $(uname -m) = "arm64" ]; then
-  arch="arm64"
-else
-  arch="amd64"
-fi
+arch=$(uname -m | sed 's/aarch64/arm64/; s/x86_64/amd64/')
 
-echo "Checking if ${feature_name} is installed..."
-if [ "${VERSION}" = "none" ] || type ${feature_name} > /dev/null 2>&1; then
-    echo "${feature_name} already installed. Skipping..."
+echo "Checking if ${binary_name} is installed..."
+if [ "${VERSION}" = "none" ] || type ${binary_name} > /dev/null 2>&1; then
+    echo "${binary_name} already installed. Skipping..."
 else
-  echo "Installing ${feature_name}..."
+  echo "Installing ${binary_name}..."
   if [ "${VERSION}" = "latest" ] ; then
-    binary_version=$(curl -sSL https://api.github.com/repos/hashicorp/${feature_name}/releases/latest | jq -r '.tag_name | split("v")[1]')
+    binary_version=$(curl -sSL https://api.github.com/repos/hashicorp/${binary_name}/releases/latest | jq -r '.tag_name | split("v")[1]')
   else
     binary_version="${VERSION}"
   fi
-  curl -sSLO https://releases.hashicorp.com/${feature_name}/${binary_version}/${feature_name}_${binary_version}_linux_${arch}.zip && unzip -jq ${feature_name}_${binary_version}_linux_${arch}.zip ${feature_name} -d /usr/local/bin/
-  rm -f ${feature_name}_${binary_version}_linux_${arch}.zip
+  curl -sSLO https://releases.hashicorp.com/${binary_name}/${binary_version}/${binary_name}_${binary_version}_linux_${arch}.zip && unzip -jq ${binary_name}_${binary_version}_linux_${arch}.zip ${binary_name} -d /usr/local/bin/
+  rm -f ${binary_name}_${binary_version}_linux_${arch}.zip
 fi
-
-ls -la /usr/local/bin/${feature_name}
-echo "${feature_name} installed successfully!"
 
 set +e
 
-echo "${feature_name} installation complete!"
+echo "${binary_name} installation complete!"
