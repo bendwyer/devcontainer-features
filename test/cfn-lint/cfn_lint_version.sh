5a1299bd9eedf36d6d8753d4cc660167d9bc9c25
bendwyer <17102207+bendwyer@users.noreply.github.com>
2026-05-19 23:43:02 +0000

    Add cfn-lint feature

---
+++ test/cfn-lint/cfn_lint_version.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+# See https://github.com/devcontainers/cli/blob/HEAD/docs/features/test.md#dev-container-features-test-lib
+# Provides the 'check' and 'reportResults' commands.
+source dev-container-features-test-lib
+
+# Feature-specific tests
+# The 'check' command comes from the dev-container-features-test-lib.
+echo ""
+echo "Running as $(whoami)"
+check "pipx version" pipx --version
+check "pipx list" pipx list
+check "cfn-lint packages" pipx runpip cfn-lint list
+check "cfn-lint location" which cfn-lint
+check "cfn-lint version" cfn-lint --version
+
+# Report result
+# If any of the checks above exited with a non-zero exit code, the test will fail.
+reportResults
